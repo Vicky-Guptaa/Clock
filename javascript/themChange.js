889d7b3fb33c2488d2137d968a277cbcad3dfe12
Vicky Gupta <vickyguptaa7@gmail.com>
2022-03-06 17:42:07 +0530

    clock most bug fix

--- javascript/themChange.js
+++ javascript/themChange.js
@@ -21,9 +21,11 @@ switchTheme.addEventListener("click", (e) => {
         changeColor(digitalTime, "text-light-bg", "text-grey-tick");
         changeColor(hourHandColor, "bg-light-bg", "bg-grey-tick");
         changeColor(minuteHandColor, "bg-light-bg", "bg-grey-tick");
-        changeColor(centerDotBorderColor, "bg-dark-bg", "bg-light-bg");
+        changeColor(centerDotBorderColor, "border-dark-bg", "border-light-bg");
         
         htmlIcon.innerHTML = "<i class='fa-solid fa-sun text-primary-accent text-2xl hover:scale-125 duration-500 ease-in-out '></i>";
+        borderColor[0].classList.remove('border-sm');
+        borderColor[0].classList.add('p-2');
     }
     else {
         changeColor(mainColor, "bg-light-bg", "bg-dark-bg");
@@ -32,7 +34,9 @@ switchTheme.addEventListener("click", (e) => {
         changeColor(hourHandColor, "bg-grey-tick", "bg-light-bg");
         changeColor(minuteHandColor,  "bg-grey-tick", "bg-light-bg");
         htmlIcon.innerHTML = "<i class='fa-solid fa-moon text-primary-accent text-2xl hover:scale-125 duration-500 ease-in-out '></i>";
-        changeColor(centerDotBorderColor, "bg-light-bg", "bg-dark-bg");
+        changeColor(centerDotBorderColor, "border-light-bg", "border-dark-bg");
+        borderColor[0].classList.add('border-sm');
+        borderColor[0].classList.remove('p-2');
     }
     isDark = !isDark;
 });
